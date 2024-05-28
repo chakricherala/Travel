680953096f22eab0ce684487be3a532a73c2b064
Chakradhar Cherala <68188570+chakricherala@users.noreply.github.com>
2024-05-28 13:47:14 +0530

    practice website

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Camp from '@/components/Camp'
+import GetApp from '@/components/GetApp'
+import Guide from '@/components/Guide'
+import Home from '@/components/Home'
+import Features from '@/components/Features'
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <Home />
+    <Camp />
+    <Guide />
+    <Features />
+    <GetApp />
+    </>
+  )
+}
+
+export default page
